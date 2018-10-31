993665215cbcf32295d04b3d8f933b395eeb5dc3
robonag <44565284+robonag@users.noreply.github.com>
2018-10-31 17:40:29 +0800

    added sitecore download, unzip, and install

--- Artifacts/windows-sitecore-xp0/artifact.ps1
+++ Artifacts/windows-sitecore-xp0/artifact.ps1
@@ -3,4 +3,16 @@ param(
     [string] $InstallationZipUrl
 )
 
-New-Item -ItemType "Directory" -Name "$InstallationZipUrl" -Path "C:\"
+New-Item -ItemType "Directory" -Name "temp" -Path "C:\"
+
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $InstallationZipUrl -Destination "C:\temp\install.zip"
+
+Expand-Archive -Path "C:\temp\install.zip" -DestinationPath "C:\temp"
+
+cd "C:\temp\Sitecore IKEA"
+
+Get-ChildItem -Recurse | Unblock-File
+
+
+iex '.\install sc9.ps1'
